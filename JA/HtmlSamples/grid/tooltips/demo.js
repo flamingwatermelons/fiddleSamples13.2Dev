1a350d4743762dc3b3caadf5d74d93ec83756a52
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2014-01-20 09:07:54 -0500

    SB build 13.2.20132.1955

--- JA/HtmlSamples/grid/tooltips/demo.js
+++ JA/HtmlSamples/grid/tooltips/demo.js
@@ -1,21 +1,14 @@
 $(function () {
             $( "#grid" ).igGrid( {
-                rowTemplate: "<tr>" +
-                    "<td> ${FirstName} </td> " +
-                    "<td> ${LastName} </td> " +
-                    "<td> ${Title} </td> " +
-                    "<td> ${BirthDate} </td> " +
-                    "<td> <p class='tooltip-grid-notes'> ${Notes} </p> </td>" +
-                "</tr>",
                 autoGenerateColumns: false,
+                autofitLastColumn: false,
                 width: "100%",
-                height: "300px",
                 columns: [
                     { headerText: "名前", key: "FirstName", dataType: "string", width: "10%" },
                     { headerText: "名字", key: "LastName", dataType: "string", width: "10%" },
-                    { headerText: "役職", key: "Title", dataType: "string", width: "15%" },
-                    { headerText: "生年月日", key: "BirthDate", dataType: "date", width: "10%" },
-                    { headerText: "Notes", key: "Notes", dataType: "string", width: "55%" }
+                    { headerText: "役職", key: "Title", dataType: "string", width: "17%" },
+                    { headerText: "生年月日", key: "BirthDate", dataType: "date", width: "8%" },
+                    { headerText: "Notes", key: "Notes", dataType: "string", width: "55%", template: "<p class='tooltip-grid-notes'> ${Notes} </p>" }
                 ],
                 dataSource: northwind,
                 responseDataKey: "results",
@@ -27,11 +20,25 @@ $(function () {
                            { columnKey: "LastName", allowTooltips: false }
                         ],
                         style: "tooltip",
-                        cursorLeftOffset: 20,
-                        cursorTopOffset: 30,
-                        visibility: "always",
-                        showDelay: 300,
-                        hideDelay: 200
+                        visibility: "always"
+                    },
+                    {
+                        name: "Responsive",
+                        enableVerticalRendering: false,
+                        columnSettings: [
+                            {
+                                columnKey: "LastName",
+                                classes: "ui-hidden-phone"
+                            },
+                            {
+                                columnKey: "Title",
+                                classes: "ui-hidden-phone"
+                            },
+                            {
+                                columnKey: "BirthDate",
+                                classes: "ui-hidden-phone"
+                            }
+                        ]
                     }
                 ]
             });
